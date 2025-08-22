f5e471940c749eb3bf99d26ed5846e1979baa004
KISHORE110504 <kishoreanbu70@gmail.com>
2025-08-22 23:31:11 +0530

    solved one medium problem

--- Medium/Number_of_Zero-Filled_Subarrays.py
+++ Medium/Number_of_Zero-Filled_Subarrays.py
@@ -1,3 +1,15 @@
 """Given an integer array nums, return the number of subarrays filled with 0.
 A subarray is a contiguous non-empty sequence of elements within an array."""
 
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        count = 0
+        total = 0
+
+        for i in nums:
+            if i == 0:
+                count += 1
+                total += count
+            else:
+                count = 0
+        return total
